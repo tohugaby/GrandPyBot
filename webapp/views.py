3a1d1d77a73b47fa067e8ded3d8ad5304dc0af28
tom.gabriele <tom.gabriele@free.fr>
2018-04-18 23:43:35 +0200

    form_base_submit
    - add js script to manage form submission
    - add post view and associated tests
    - integrate js into index.html

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/process", methods=["POST"])
+def process():
+    search_terms = ""
+    if "search" in request.form:
+        search_terms = request.form["search"]
+    return jsonify(dict(search=search_terms))
