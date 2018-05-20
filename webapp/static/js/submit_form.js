3b064ba21fceea8b3a6b1848450937a250e8821e
tom.gabriele <tom.gabriele@free.fr>
2018-05-21 00:18:00 +0200

    loaderjs
    - add js, css and html to display a loader when searching

--- webapp/static/js/submit_form.js
+++ webapp/static/js/submit_form.js
@@ -1,7 +1,36 @@
 const processUrl = "/process";
 let mapDiv = document.getElementById("map");
+let dialogDiv = document.getElementById("dialog");
 let addressDiv = document.getElementById("address");
 let descriptionDiv = document.getElementById("description");
+let loaderDiv = document.getElementsByClassName("loader")[0];
+let loaderText = loaderDiv.getElementsByClassName("loader-text")[0];
+let initialLoaderText = loaderText.innerHTML;
+console.log(loaderText);
+let colors = ['#c62e1a', '#13b5db', 'yellow'];
+let loadInterval = null;
+let loader = (loading) => {
+        let i = 0;
+        if (loading === true) {
+            loaderDiv.classList.remove("hide-me");
+            loadInterval = setInterval(() => {
+                dialogDiv.style.backgroundColor = colors[i];
+                if (i < colors.length) {
+                    i++;
+                    loaderText.innerHTML += "...";
+                } else {
+                    i = 0;
+                }
+            }, 1000)
+        } else {
+            clearInterval(loadInterval);
+            loaderDiv.classList.add("hide-me");
+            dialogDiv.style.backgroundColor = '#13b5db'
+            loaderText.innerHTML = initialLoaderText;
+        }
+    }
+;
+
 
 function myMap(lat, long) {
     let center = new google.maps.LatLng(lat, long);
@@ -22,8 +51,10 @@ let submitForm = (url, data, callback) => {
     req.addEventListener("load", () => {
         if (req.status >= 200 && req.status < 400) {
             callback(req.responseText);
+            loader(false);
         } else {
             console.error(req.status + " " + req.statusText + " " + url)
+            loader(false);
         }
     });
     req.addEventListener("error", () => {
@@ -35,6 +66,7 @@ let submitForm = (url, data, callback) => {
 
 let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", (e) => {
+    loader(true);
     mapDiv.style.height = '0';
     let data = new FormData(searchForm);
     console.log(searchForm);
